79455f61838c43705998597d46cf5679967d2d61
hunkaracr1 <114934461+hunkaracar@users.noreply.github.com>
2022-10-16 16:57:42 +0300

    Add files via upload

---
+++ employeeodev/employeeodev/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employeeodev
+{
+    public class Employee
+    {
+        public int Age{ get; set; }
+        public string Employees{ get; set; }
+        public string FirstName { get; set; }   
+        public int Id { get; set; } 
+        public string LastName { get; set; }
+
+
+
+        public Employee()
+        {
+            Console.WriteLine("Employee class initilazie..");
+        }
+
+        
+
+        public Employee(int age, string employees, string firstName, int id, string lastName)
+        {
+            Age = age;
+            Employees = employees;
+            FirstName = firstName;
+            Id = id;
+            LastName = lastName;
+        }
+
+        public override string? ToString()
+        {
+            return $"{Age} {Employees} {FirstName} {Id} {LastName}";
+        }
+
+
+
+
+
+    }
+
+
+}
---
+++ employeeodev/employeeodev/Program.cs
@@ -0,0 +1,30 @@
+﻿using employeeodev;
+
+namespace employeeodev;
+
+
+class program
+{
+    public static void Main()
+    {
+        Employee employee = new Employee(19,"Hunkar","arda",0,"Acar");
+
+        Addd ad = new Addd();
+
+        ad.CreateAdd(employee);
+
+        Console.WriteLine(ad.ReadId(0));
+
+        ad.CreateAdd(employee);
+
+        
+
+
+
+
+
+
+    }
+
+    
+}
